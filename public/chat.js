bbedccf6467f6b6cb03d1de6800d641a6a56bf1e
Solrac <carlos.ornelas@gmail.com>
2019-11-23 14:42:26 -0800

    Functional
    works but i need to fix the ugly UI....

--- public/chat.js
+++ public/chat.js
@@ -4,6 +4,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
     //MAKE SURE SERVER IS RUNNING ON PORT 8000 
 
     const socket = io.connect('http://localhost:8000');
+
+    var messageAppend = document.getElementById('messageBox');
+    var messageBox = document.getElementById('userMessage');
     
 
     var sendButton = document.getElementById('myBtn');
@@ -20,16 +23,19 @@ window.addEventListener('DOMContentLoaded', (event) => {
             userMessage
         });
 
+        messageBox.value = "";
+
     })
 
     socket.on('messagesToAll', (data)=>{
-        
+        messageAppend.innerHTML += '<p>' + data.userHandle + ':<span>' + data.userMessage + '</span></p>'
+        // messageAppend.append(`${data.userHandle}: ${data.userMessage}`)
     })
 
+    // messageAppend.innerHTML = '<p>';
 
 
-
-
+    // END OF CONTENT LOADED
 });
 
 
